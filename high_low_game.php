a1c2176ad8ddead0c1800c07deeef336c5f9a5e8
David R. Jex <david.jex@me.com>
2017-07-19 14:59:23 -0500

    Added counter for guessed made.

--- high_low_game.php
+++ high_low_game.php
@@ -1,7 +1,8 @@
 <?php
 
 // game picks a random number between 1 and 100.
-$computersNumber = rand(1, 100);
+$computersNumber = mt_rand(1, 100);
+$numberOfGuesses = 0;
 // prompts user to guess the number
 
 // if user's guess is less than the number, it outputs "HIGHER"
@@ -12,10 +13,14 @@ $computersNumber = rand(1, 100);
 		fwrite(STDOUT, "PLEASE ENTER A VALID INTEGER\n");
 	} else if ($usersNumber > $computersNumber) {
 		fwrite(STDOUT, "LOWER\n");
+		$numberOfGuesses += 1;
 	} else if ($usersNumber == $computersNumber) {
 		fwrite(STDOUT, "GOOD GUESS\n");
+		$numberOfGuesses += 1;
+		fwrite(STDOUT, "You guessed {$numberOfGuesses} times.\n");
 	} else if ($usersNumber < $computersNumber) {
 		fwrite(STDOUT, "HIGHER\n");
+		$numberOfGuesses += 1;
 	}
 } while ($usersNumber != $computersNumber);
 // if user's guess is more than the number, it outputs "LOWER"
